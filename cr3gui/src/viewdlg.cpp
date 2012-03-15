bf0add0ac6506f066e7b593629f54c0a3a480292
varnie <varnie29a@mail.ru>
2012-03-15 22:26:47 +0600

    renamed 'Close book' button.

--- cr3gui/src/viewdlg.cpp
+++ cr3gui/src/viewdlg.cpp
@@ -538,7 +538,7 @@ static const char * getCommandName( int command )
 	case MCMD_SELECT: return ("Ok");
 	case MCMD_SCROLL_FORWARD: return _("Scroll forward");
 	case MCMD_SCROLL_BACK: return _("Scroll back");
-	case MCMD_QUIT: return _("Close book");
+	case MCMD_QUIT: return _("Close application");
 	case MCMD_MAIN_MENU: return _("Main menu");
 	case MCMD_GO_PAGE: return _("Go to page dialog");
 	case MCMD_SETTINGS: return _("Settings menu");
